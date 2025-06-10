333eb873180550b0510689f2694c0a6091ebcfc7
Nic <nco63@uclive.ac.nz>
2025-06-10 16:08:47 +1200

    working navigation

--- Neural Chain/Assets/Scripts/GameManager.cs
+++ Neural Chain/Assets/Scripts/GameManager.cs
@@ -1,17 +1,49 @@
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
     public Camera cam;
     public LayerMask groundMask;           // Only raycast against ground
+    public GameObject playerAgent;
+    public GameObject enemyAgent;
 
     public float navMeshSampleMaxDistance = 10f;
+
+    public Transform[] playerSpawnPoints;
+    public Transform[] enemySpawnPoints;
+
+    private List<GameObject> playerAgents = new List<GameObject>();
+    private List<GameObject> enemyAgents = new List<GameObject>();
+    private GameObject currentPlayerAgent;
 
     void Update()
     {
         CheckClick();
+    }
+
+    void Start()
+    {
+        SpawnAgents();
+    }
+
+    private void SpawnAgents()
+    {
+        for (int i = 0; i < playerSpawnPoints.Length; i++)
+        {
+            Transform spawnPos = playerSpawnPoints[i % playerSpawnPoints.Length];
+            GameObject agent = Instantiate(playerAgent, spawnPos.position, spawnPos.rotation);
+            playerAgents.Add(agent);
+        }
+
+        for (int i = 0; i < enemySpawnPoints.Length; i++)
+        {
+            Transform spawnPos = enemySpawnPoints[i % enemySpawnPoints.Length];
+            GameObject agent = Instantiate(enemyAgent, spawnPos.position, spawnPos.rotation);
+            enemyAgents.Add(agent);
+        }
     }
 
     private void CheckClick()
@@ -20,15 +52,26 @@ public class GameManager : MonoBehaviour
         {
             Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
 
-            if (Physics.Raycast(ray, out RaycastHit hit, 100f, groundMask))
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f))
             {
-                // Optional: Debug click
-                Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 1f);
+                GameObject clickedObject = hit.collider.gameObject;
 
-                // Find the nearest point on the NavMesh to where we clicked
-                if (NavMesh.SamplePosition(hit.point, out NavMeshHit navHit, navMeshSampleMaxDistance, NavMesh.AllAreas))
+                if (clickedObject.CompareTag("PlayerAgent"))
                 {
-                    Debug.Log(navHit.position);
+                    currentPlayerAgent = clickedObject;
+                    return;
+                }
+            }
+
+            if (Physics.Raycast(ray, out RaycastHit groundHit, 100f, groundMask))
+            {
+                if (NavMesh.SamplePosition(groundHit.point, out NavMeshHit navHit, navMeshSampleMaxDistance, NavMesh.AllAreas))
+                {
+                    if (currentPlayerAgent != null)
+                    {
+                        NavMeshAgent agent = currentPlayerAgent.GetComponent<NavMeshAgent>();
+                        agent.SetDestination(navHit.position);
+                    }
                 }
             }
         }
